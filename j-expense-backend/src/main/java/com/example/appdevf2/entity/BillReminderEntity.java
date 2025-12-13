0e0beb0078f2a661b7a8b7bc4bcdfc0e70b06940
jones21312321313213 <skinnyjones212121@gmail.com>
2025-12-14 01:03:55 +0800

    changed things in entity folder to match userEntity(wala pa categoryu

--- j-expense-backend/src/main/java/com/example/appdevf2/entity/BillReminderEntity.java
+++ j-expense-backend/src/main/java/com/example/appdevf2/entity/BillReminderEntity.java
@@ -1,9 +1,21 @@
 package com.example.appdevf2.entity;
 
-import jakarta.persistence.*;
-
 import java.util.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Entity
 @Table(name = "tbl_bills")
 public class BillReminderEntity {
@@ -17,28 +29,32 @@ public class BillReminderEntity {
     private Date dueDate;
     private boolean status;
 
-    public BillReminderEntity() {super();}
-    public BillReminderEntity(int billID, String billName, double amount, Date dueDate, boolean status) {
-        super();
-        this.billID = billID;
-        this.billName = billName;
-        this.amount = amount;
-        this.dueDate = dueDate;
-        this.status = status;
-    }
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
 
-    public int getBillID() { return billID; }
-    public void setBillID(int billID) { this.billID = billID; }
+    // public BillReminderEntity() {super();}
+    // public BillReminderEntity(int billID, String billName, double amount, Date dueDate, boolean status) {
+    //     super();
+    //     this.billID = billID;
+    //     this.billName = billName;
+    //     this.amount = amount;
+    //     this.dueDate = dueDate;
+    //     this.status = status;
+    // }
 
-    public String getBillName() { return billName; }
-    public void setBillName(String billName) { this.billName = billName; }
+    // public int getBillID() { return billID; }
+    // public void setBillID(int billID) { this.billID = billID; }
 
-    public double getAmount() { return amount; }
-    public void setAmount(double amount) { this.amount = amount; }
+    // public String getBillName() { return billName; }
+    // public void setBillName(String billName) { this.billName = billName; }
 
-    public Date getDueDate() { return dueDate; }
-    public void setDueDate(Date dueDate) { this.dueDate = dueDate; }
+    // public double getAmount() { return amount; }
+    // public void setAmount(double amount) { this.amount = amount; }
 
-    public boolean isStatus() { return status; }
-    public void setStatus(boolean status) { this.status = status; }
+    // public Date getDueDate() { return dueDate; }
+    // public void setDueDate(Date dueDate) { this.dueDate = dueDate; }
+
+    // public boolean isStatus() { return status; }
+    // public void setStatus(boolean status) { this.status = status; }
 }
--- j-expense-backend/src/main/java/com/example/appdevf2/entity/RecurringTransactionEntity.java
+++ j-expense-backend/src/main/java/com/example/appdevf2/entity/RecurringTransactionEntity.java
@@ -1,13 +1,19 @@
 package com.example.appdevf2.entity;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.Date;
-import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -37,6 +43,10 @@ public class RecurringTransactionEntity {
     @JsonIgnoreProperties({"recurringTransactions"}) 
     private TransactionEntity transaction;
 
+
+    @ManyToOne
+    @JoinColumn(name = "user_id") // FK column
+    private UserEntity user; 
 
 
     // public RecurringTransactionEntity() {
